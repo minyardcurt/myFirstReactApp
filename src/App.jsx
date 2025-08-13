9be0017070009e14b67f5e4fd14382cbe912a810
CurtMinyard <cminyard@perseverenow.org>
2025-08-13 10:20:02 -0500

    redo commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Body from './components/Body';
 import Footer from './components/Footer';
+import './App.css'; 
 
 function App() {
   return (
-    <div>
+    <div className="app-container">
       <Header />
-      <div>
+      <div className="main-section">
         <Sidebar />
         <Body />
       </div>
